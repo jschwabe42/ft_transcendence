4586c52b4fdfd563017005b9c92b839e2f413139
jschwabe <mail@jschwabe.site>
2024-10-31 14:14:16 +0100

    chore(django tutorial 3 - WIP): Removing hardcoded URLs in templates
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -5,8 +5,13 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
+    # the 'name' value as called by the {% url %} template tag
     path("<int:question_id>/", views.detail, name="detail"),
+    # changing url of polls detail view to something else:
+    # perhaps to something like polls/specifics/1/
+    # instead of doing it in the template (or templates)
+    # you would change it here: @note
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/results
