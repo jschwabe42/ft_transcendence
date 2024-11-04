a564f7224edccd1a01571a13c1498c7ffcb62cb2
Jonstep101010 <mail@jschwabe.site>
2024-11-04 23:30:00 +0100

    chore(django tutorial 4): Use generic views
    Less code is better

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -4,17 +4,8 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # the 'name' value as called by the {% url %} template tag
-    path("<int:question_id>/", views.detail, name="detail"),
-    # changing url of polls detail view to something else:
-    # perhaps to something like polls/specifics/1/
-    # instead of doing it in the template (or templates)
-    # you would change it here: @note
-    path("specifics/<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/results
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
