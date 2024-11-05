55820c07267720253eb4341531eecfd27081e91b
Jonstep101010 <mail@jschwabe.site>
2024-11-05 11:17:07 +0100

    chore(django tutorial 7): Adding related objects

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # number of extra slots for related model
+    extra = 3
+
+# create model admin class, then pass it as second arg to register
+# any time we want to customize admin options for a model
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]# question text now appears above pub date
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
