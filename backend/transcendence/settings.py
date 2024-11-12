1b7d3fb52c55838b73e96998ae75eac72d507b80
jschwabe <mail@jschwabe.site>
2024-11-12 19:59:43 +0100

    chore: remove DATABASES default placeholder (sqlite3)
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- backend/transcendence/settings.py
+++ backend/transcendence/settings.py
@@ -91,10 +91,6 @@ WSGI_APPLICATION = 'transcendence.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
