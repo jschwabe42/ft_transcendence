40ae5331e9d4188caf1fa321e565a4e519b45529
jschwabe <mail@jschwabe.site>
2024-11-18 19:24:53 +0100

    chore(player): register views
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- backend/pong/urls.py
+++ backend/pong/urls.py
@@ -5,4 +5,6 @@ app_name = "pong"
 urlpatterns = [
     path("", views.recent_games, name="recent_games"),
     path("<int:game_id>/", views.game_details, name="game_details"),
+    path("players/", views.players, name="players"),
+    path("players/<int:player_id>/", views.player_details, name="player_details"),
 ]
--- backend/pong/views.py
+++ backend/pong/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
-from .models import Game
+from .models import Game, Player
 
 
 # show recent games for now
@@ -12,3 +12,10 @@ def recent_games(request):
 def game_details(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
     return render(request, "pong/game_details.html", {"game": game})
+
+def players(request):
+    return render(request, "pong/players.html", {"players_list": Player.objects.order_by("-created_at")[:10]})
+
+def player_details(request, player_id):
+    player = get_object_or_404(Player, pk=player_id)
+    return render(request, "pong/player_details.html", {"player": player})
