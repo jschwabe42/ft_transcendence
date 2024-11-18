1b1c49c07a1d4294ff9ad911a5aaeedce83e4d61
jschwabe <mail@jschwabe.site>
2024-11-18 19:24:31 +0100

    chore(player): register admin
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- backend/pong/admin.py
+++ backend/pong/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Game
+from .models import Game, Player
 
 admin.site.register(Game)
+admin.site.register(Player)
