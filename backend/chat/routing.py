9f214ed4263c615aecdb7fcd7b263f86bc8be54e
jschwabe <mail@jschwabe.site>
2024-11-26 18:17:48 +0100

    chore: cleanup

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Beispiel: URL für einen Chatraum mit einer room_id
+    # example: URL for chatroom with room_id
     re_path(r'ws/chat/(?P<room_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
