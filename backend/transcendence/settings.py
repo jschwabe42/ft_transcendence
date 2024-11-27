36d4d02ad9f569ce2cc5732e96c4cb56400a262f
Eugen <118663096+itseugen@users.noreply.github.com>
2024-11-27 15:39:26 +0000

    Replaced the navbar with a universal navigation bar

--- backend/transcendence/settings.py
+++ backend/transcendence/settings.py
@@ -80,7 +80,8 @@ ROOT_URLCONF = 'transcendence.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # This line should point to the 'templates' directory
+        # 'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
