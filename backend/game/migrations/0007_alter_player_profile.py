9e3f771cfcd360f641eec24f8dbef2febb05ef80
jschwabe <mail@jschwabe.site>
2024-12-22 19:51:42 +0100

    chore(game): make squashed new initial

--- backend/game/migrations/0007_alter_player_profile.py
+++ backend/game/migrations/0007_alter_player_profile.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('game', '0006_alter_player_profile'),
+        ('game', '0001_initial'),
         ('users', '0001_initial'),
     ]
 
