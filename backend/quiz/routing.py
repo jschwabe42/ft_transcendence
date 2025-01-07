0e8d08498f11aa4723ae3cabffe1ce4b213c2faa
Eugen <118663096+itseugen@users.noreply.github.com>
2025-01-07 12:04:56 +0000

    deleted some shit maybe work?

--- backend/quiz/routing.py
+++ backend/quiz/routing.py
@@ -2,6 +2,4 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path(r'ws/quiz/room/(?P<room_name>\w+)/$', consumers.QuizConsumer.as_asgi()),
-	re_path(r'ws/quiz/home/$', consumers.RoomListConsumer.as_asgi()),
 ]
