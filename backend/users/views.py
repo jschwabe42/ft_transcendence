58f17a2bffd4fcf1eeecaf284eb2f6e635ee96ed
jschwabe <mail@jschwabe.site>
2025-01-13 20:44:48 +0100

    chore: order games displayed in public profile, formatting

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,17 +11,17 @@ from .models import Profile, User
 
 
 def register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()  # Only saves the User instance; Profile creation is handled by the signal
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can now log in!')
-            return redirect('login')
-    else:
-        form = UserRegisterForm()
-    
-    return render(request, 'users/register.html', {'form': form})
+	if request.method == 'POST':
+		form = UserRegisterForm(request.POST)
+		if form.is_valid():
+			user = form.save()  # Only saves the User instance; Profile creation is handled by the signal
+			username = form.cleaned_data.get('username')
+			messages.success(request, f'Your account has been created! You can now log in!')
+			return redirect('login')
+	else:
+		form = UserRegisterForm()
+	
+	return render(request, 'users/register.html', {'form': form})
 
 
 
@@ -31,35 +31,35 @@ from django.contrib.auth.forms import PasswordChangeForm
 
 #costum Logout, couse Idk I am stupid to get the normal working
 def custom_logout(request):
-    logout(request)  # Logs out the user
-    return render(request, 'users/logout.html') # Redirects the user to the login page
+	logout(request)  # Logs out the user
+	return render(request, 'users/logout.html') # Redirects the user to the login page
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-        mod_pwd_form = PasswordChangeForm(request.user, request.POST)
-        if u_form.is_valid() and p_form.is_valid() and mod_pwd_form.is_valid():
-            u_form.save()
-            p_form.save()
-            user = mod_pwd_form.save()
-            update_session_auth_hash(request, user)
-            messages.success(request, f'Your account has been updated')
-            return redirect('profile')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+		mod_pwd_form = PasswordChangeForm(request.user, request.POST)
+		if u_form.is_valid() and p_form.is_valid() and mod_pwd_form.is_valid():
+			u_form.save()
+			p_form.save()
+			user = mod_pwd_form.save()
+			update_session_auth_hash(request, user)
+			messages.success(request, f'Your account has been updated')
+			return redirect('profile')
 
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-        mod_pwd_form = PasswordChangeForm(request.user)
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+		mod_pwd_form = PasswordChangeForm(request.user)
 
-    context = {
-        'u_form': u_form,
-        'p_form': p_form,
-        'mod_pwd_form': mod_pwd_form,
-    }
+	context = {
+		'u_form': u_form,
+		'p_form': p_form,
+		'mod_pwd_form': mod_pwd_form,
+	}
 
-    return render(request, 'users/profile.html', context)
+	return render(request, 'users/profile.html', context)
 
 from django.views.generic import ListView, DetailView
 from django.db.models import F
@@ -70,9 +70,11 @@ from django.db.models import F
 #     return render(request, 'users/public_profile.html', context)
 
 def public_profile(request, query_user):
-    user_instance = User.objects.get(username=query_user)
-    user_profile = Profile.objects.get(user=user_instance)
-    games = Game.objects.filter(player1=user_profile.player) | Game.objects.filter(player2=user_profile.player)
-    games_won = games.filter(player1=user_profile.player, score1__gt=F('score2')) | games.filter(player2=user_profile.player, score2__gt=F('score1'))
-    games_lost = [game for game in games if game not in games_won]
-    return render(request, 'users/public_profile.html', {'user_profile': user_profile, 'games': games, 'games_won': games_won, 'games_lost': games_lost})
+	user_instance = User.objects.get(username=query_user)
+	user_profile = Profile.objects.get(user=user_instance)
+	games = Game.objects.filter(player1=user_profile.player) | Game.objects.filter(player2=user_profile.player)
+	games_won = games.filter(player1=user_profile.player, score1__gt=F('score2')) | games.filter(player2=user_profile.player, score2__gt=F('score1'))
+	games_lost = [game for game in games if game not in games_won]
+	games_won = sorted(games_won, key=lambda game: game.played_at, reverse=True)
+	games_lost = sorted(games_lost, key=lambda game: game.played_at, reverse=True)
+	return render(request, 'users/public_profile.html', {'user_profile': user_profile, 'games': games, 'games_won': games_won, 'games_lost': games_lost})
