a2ed015f483af52fd122e49512bff78f21c5c481
jschwabe <mail@jschwabe.site>
2025-01-17 17:39:13 +0100

    WIP: handling friend requests

--- backend/users/models.py
+++ backend/users/models.py
@@ -50,6 +50,13 @@ class Friends(models.Model):
 	def __eq__(self, other):
 		return (self.origin, self.target) == (other.origin, other.target)
 
+	def has_target(self, some_user):
+		return self.target == some_user
+	
+	def has_origin(self, some_user):
+		return self.origin == some_user
+		
+
 # need to always check origin user of a request
 class Friends_Manager(models.Model):
 	friendships_active = set()
@@ -57,6 +64,8 @@ class Friends_Manager(models.Model):
 
 	# origin should be the request user, target is their request
 	def friends_request(self, origin, string_target_friend):
+		if not User.objects.exists(string_target_friend):
+			raise ValidationError('the user you are trying to befriend does not exist!')
 		target_friend = User.objects.get(string_target_friend)
 		if target_friend == origin:
 			raise ValidationError('You cannot befriend yourself!')
@@ -64,9 +73,9 @@ class Friends_Manager(models.Model):
 		if current_set not in self.friendships_active and current_set not in self.friendships_inactive:
 			reversed_set = Friends(target_friend, origin)
 			if reversed_set not in self.friendships_active and reversed_set not in self.friendships_inactive:
-			self.friendships_inactive.add(Friends(origin, target_friend))
+				self.friendships_inactive.add(Friends(origin, target_friend))
 			elif reversed_set not in self.friendships_active and reversed_set in self.friendships_inactive:
-				# handle auto-accept if we are the target? (reversed_set in inactive) @follow-up
+				# handle redirect to accept as the target? (reversed_set in inactive) @follow-up
 				raise ValidationError('there is already a friends request from this user for you to accept.')
 			else:
 				raise ValidationError('there is already a request or friendship in between these users')
@@ -75,21 +84,28 @@ class Friends_Manager(models.Model):
 		elif current_set in self.friendships_active:
 			raise ValidationError('You are already friends with this user!')
 
-	# is this ok like this? @follow-up
-	def cancel_or_deny_request(self, origin_or_target):
+	# cancel the request as origin
+	def cancel_friends_request(self, origin):
+		"""User instance"""
 		for friendship in self.friendships_inactive:
-			if friendship.acontains(origin_or_target):
+			if friendship.has_origin(origin):
 				self.friendships_inactive.remove(friendship)
 
-	# this needs more work @todo
-	# What does this take? the User having to accept or Friends' instance
-	def accept_request_as_target(self, target_friend):
+	def deny_friends_request(self, target):
+		"""User instance: deny"""
 		for friendship in self.friendships_inactive:
-			if friendship.acontains(target_friend):
+			if friendship.has_target(target):
+				self.friendships_inactive.remove(friendship)
+
+	def accept_request_as_target(self, target_friend):
+		"""User instance: accept"""
+		for friendship in self.friendships_inactive:
+			if friendship.has_target(target_friend):
 				origin, destination = friendship
-				if destination == target_friend:
-					self.friendships_active.add(Friends(origin, target_friend))
-					self.friendships_inactive.remove(friendship)
+				self.friendships_active.add(Friends(origin, destination))
+				self.friendships_inactive.remove(friendship)
+
+	# @todo some way to check for outstanding friend requests/instances
 
 	# taking an instance seems quite logical
 	def accept_request_with_instance(self, maybe_friendship):
