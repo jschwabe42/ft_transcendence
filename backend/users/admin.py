97a395f53e8a5f2a76878ef2d44bc4fc9b98a5b4
Jonstep101010 <mail@jschwabe.site>
2025-01-20 22:07:11 +0100

    chore: remove admin usage of `Friends`

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Profile, Friends, Friends_Manager
+from .models import Profile
 
 admin.site.register(Profile)
-admin.site.register(Friends)
-# admin.site.register(Friends_Manager)# causes TypeError
+
 
 # Register your models here.
