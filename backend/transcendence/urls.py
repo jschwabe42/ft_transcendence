5ba8b76b0f406bd8ef779227f46421d8b6022437
jschwabe <mail@jschwabe.site>
2025-01-22 13:48:45 +0100

    chore: make homepage be players/users list

--- backend/transcendence/urls.py
+++ backend/transcendence/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
 
 	path('game/', include("game.urls")),
 	path('admin/', admin.site.urls),
+	path('', players_pong, name='players'),
 	path('', include('blog.urls')),
 	path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 	path('chat/', include('chat.urls')),
