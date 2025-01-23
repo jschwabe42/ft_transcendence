a92095e1d89d577928c3400929cb4d0599295bf7
jschwabe <mail@jschwabe.site>
2025-01-23 16:16:05 +0100

    chore: cleanup non-needed in asgi.py

--- backend/transcendence/asgi.py
+++ backend/transcendence/asgi.py
@@ -8,23 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
-
-application = get_asgi_application()
-
-
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
-
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter , URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from chat import routing as chat_routing
 from game import routing as game_routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transcendence.settings')
 
 application = ProtocolTypeRouter(
     {
