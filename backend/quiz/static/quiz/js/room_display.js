780a8277d774157edf4597b9977ccaa7e6600e06
Eugen <118663096+itseugen@users.noreply.github.com>
2025-01-24 13:48:14 +0000

    slightly modified the leave button again

--- backend/quiz/static/quiz/js/room_display.js
+++ backend/quiz/static/quiz/js/room_display.js
@@ -12,7 +12,7 @@ export function displayRoom(roomName) {
 		<h2 id="room-header">Welcome to ${roomName}</h2>
 		<p id="room-description">You have successfully joined the room!<br>Here you can start participating in the quiz.</p>
 		<ul id="participants-list"></ul>
-		<button id="leave-room-button" class="btn btn-danger">
+		<button id="leave-room-button" class="btn btn-danger" title="Leave Room">
 			<i class="bi bi-door-open-fill" aria-hidden="true"></i>
 			<span class="sr-only">Leave Room</span>
 		</button>
