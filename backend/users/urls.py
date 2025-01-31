f8706ee716bead544d0cebc30016145825e0854a
jschwabe <mail@jschwabe.site>
2025-01-31 18:20:22 +0100

    chore: fix breakage

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path, re_path
 from django.contrib.auth import views as auth_views
+from django.urls import path, re_path
+
 from . import views
 
 app_name = 'users'
@@ -40,15 +41,31 @@ urlpatterns = [
 		views.remove_friend,
 		name='remove-friend',
 	),
-	# WIP: oauth with 42 API
-	path('oauth/', CreateOAUTHUserView.authorize_api_user, name='oauth'),
-]
-
-	# friendship management: both `user` and `users` prefix 
+	# friendship management: both `user` and `users` prefix
 	re_path(r'^user(s)?/(?P<query_user>[^/]+)$', views.public_profile, name='public-profile'),
-	re_path(r'^user(s)?/(?P<target_username>[^/]+)/friend-request$', views.friend_request, name='friend-request'),
-    re_path(r'^user(s)?/(?P<target_username>[^/]+)/cancel-friend-request$', views.cancel_friend_request, name='cancel-friend-request'),
-    re_path(r'^user(s)?/(?P<origin_username>[^/]+)/deny-friend-request$', views.deny_friend_request, name='deny-friend-request'),
-    re_path(r'^user(s)?/(?P<origin_username>[^/]+)/accept-friend-request$', views.accept_friend_request, name='accept-friend-request'),
-    re_path(r'^user(s)?/(?P<other_username>[^/]+)/remove-friend$', views.remove_friend, name='remove-friend'),
+	re_path(
+		r'^user(s)?/(?P<target_username>[^/]+)/friend-request$',
+		views.friend_request,
+		name='friend-request',
+	),
+	re_path(
+		r'^user(s)?/(?P<target_username>[^/]+)/cancel-friend-request$',
+		views.cancel_friend_request,
+		name='cancel-friend-request',
+	),
+	re_path(
+		r'^user(s)?/(?P<origin_username>[^/]+)/deny-friend-request$',
+		views.deny_friend_request,
+		name='deny-friend-request',
+	),
+	re_path(
+		r'^user(s)?/(?P<origin_username>[^/]+)/accept-friend-request$',
+		views.accept_friend_request,
+		name='accept-friend-request',
+	),
+	re_path(
+		r'^user(s)?/(?P<other_username>[^/]+)/remove-friend$',
+		views.remove_friend,
+		name='remove-friend',
+	),
 ]
