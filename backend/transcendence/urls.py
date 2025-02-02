0836b67de1b6fa4f54766d0fb16af5ffed7d911f
Eugen <118663096+itseugen@users.noreply.github.com>
2025-02-02 18:59:43 +0000

    IMAGES WORK AGAIN OMFG WOOO

--- backend/transcendence/urls.py
+++ backend/transcendence/urls.py
@@ -30,8 +30,6 @@ urlpatterns = [
 	path('chat/', include('chat.urls')),
 	path('quiz/', include('quiz.urls')),
 	path('dashboard/', include('dashboard.urls')),
-    # path('__debug__/', include(debug_toolbar.urls)),
-	re_path(r'^.*$', views.index, name='index'),
 ]
 
 
@@ -45,3 +43,7 @@ from django.conf.urls.static import static
 # we do not know how this has to be implemented for release
 if settings.DEBUG:
 	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+		re_path(r'^.*$', views.index, name='index'),
+]
