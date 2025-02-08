6b01e4cbe9e5144828d38c301f1096a1b40ae88f
Eugen <118663096+itseugen@users.noreply.github.com>
2025-02-08 16:09:33 +0000

    Fixed the translation javascript

--- backend/transcendence/urls.py
+++ backend/transcendence/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.conf.urls.i18n import JavaScriptCatalog
+from django.views.i18n import JavaScriptCatalog
 
 from . import views
 
