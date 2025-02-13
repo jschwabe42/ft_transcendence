9f386fa02a206008740a9306fc21d01226d27bf0
Jonstep101010 <git@jschwabe.site>
2025-02-13 14:42:09 +0100

    chore: remove deprecated paths, extract friends endpoints to urlpattern

--- backend/user_management/urls.py
+++ backend/user_management/urls.py
@@ -1,44 +1,29 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 from .api_views import OauthCallBackView, OauthView
 
 app_name = 'users'
+
+# urls under `/users/api/friends/`
+friends_urls = [
+	path('active/', views.friends_users_active, name='friends'),
+	path('active/<str:username>/', views.friends_users_active, name='friends-public'),
+	path('inactive/', views.friends_requests, name='friends-requests'),
+	path('request/<str:username>/', views.friends_send_request, name='friends-send-request'),
+	path('cancel/<str:username>/', views.friends_cancel, name='friends-cancel-request'),
+	path('accept/<str:username>/', views.friends_accept, name='friends-accept-request'),
+	path('deny/<str:username>/', views.friends_deny, name='friends-deny-request'),
+	path('remove/<str:username>/', views.friends_remove, name='friends-remove'),
+]
+
 urlpatterns = [
 	path('api/oauth/', OauthView.as_view(), name='oauth'),
 	path('api/oauth-callback/', OauthCallBackView.as_view(), name='api-callback'),
 	path('api/blocked/', views.blocked_users, name='blocked_users'),
 	path('api/block/<str:username>/', views.block_user, name='block_user'),
 	path('api/unblock/<str:username>/', views.unblock_user, name='unblock_user'),
-	# friends: endpoints
-	path('api/friends/active/', views.friends_users_active, name='friends'),
-	path('api/friends/active/<str:username>/', views.friends_users_active, name='friends-public'),
-	path('api/friends/inactive/', views.friends_requests, name='friends-requests'),
-	path(
-		'api/friends/request/<str:username>/',
-		views.friends_send_request,
-		name='friends-send-request',
-	),
-	path(
-		'api/friends/cancel/<str:username>/',
-		views.friends_cancel,
-		name='friends-cancel-request',
-	),
-	path(
-		'api/friends/accept/<str:username>/',
-		views.friends_accept,
-		name='friends-accept-request',
-	),
-	path(
-		'api/friends/deny/<str:username>/',
-		views.friends_deny,
-		name='friends-deny-request',
-	),
-	path(
-		'api/friends/remove/<str:username>/',
-		views.friends_remove,
-		name='friends-remove',
-	),
+	path('api/friends/', include(friends_urls)),
 	path('api/register/', views.register, name='register'),
 	path('api/login/', views.login_view, name='login'),
 	path('api/logout/', views.logout_view, name='logout'),
@@ -48,22 +33,4 @@ urlpatterns = [
 	path('api/check_authentication/', views.check_authentication, name='check-authentication'),
 	# TODO: remove once dashboard is feature complete @follow-up
 	path('<str:query_user>', views.public_profile, name='public-profile'),
-	# friendship management: endpoints will be deprecated in favor of `api` prefix
-	path('<str:target_username>/friend-request', views.friend_request, name='friends-request'),
-	path(
-		'<str:target_username>/cancel-friend-request',
-		views.cancel_friend_request,
-		name='friends-cancel',
-	),
-	path(
-		'<str:origin_username>/deny-friend-request',
-		views.deny_friend_request,
-		name='friends-deny',
-	),
-	path(
-		'<str:origin_username>/accept-friend-request',
-		views.accept_friend_request,
-		name='friends-accept',
-	),
-	path('<str:other_username>/remove-friend', views.remove_friend, name='friends-remove'),
 ]
