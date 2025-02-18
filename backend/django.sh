618a12232017bda99aaf93577f44eaf28f55e81f
Eugen <118663096+itseugen@users.noreply.github.com>
2025-02-18 17:10:01 +0000

    Added the new certificate location

--- backend/django.sh
+++ backend/django.sh
@@ -8,4 +8,4 @@ python manage.py compilemessages
 python manage.py makemigrations
 python manage.py migrate
 
-daphne -e ssl:8000:privateKey=dev.key:certKey=dev.crt transcendence.asgi:application
+daphne -e ssl:8000:privateKey=certs/dev.key:certKey=certs/dev.crt transcendence.asgi:application
