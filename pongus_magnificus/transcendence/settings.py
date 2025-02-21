57e19b9d934fc45c7dde3db00c67163bf9cf431a
Jonstep101010 <git@jschwabe.site>
2025-02-21 20:59:55 +0100

    fix: secret key generation and docker secret integration

--- pongus_magnificus/transcendence/settings.py
+++ pongus_magnificus/transcendence/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_0@qbs8*u@@s(#=4@e8yol-y4spd)%ymko!-ja^#fs=jofyf!)'
+SECRET_KEY = Path('/var/run/secrets/SECRET_KEY').read_text().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
